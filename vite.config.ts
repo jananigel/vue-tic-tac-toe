f6b74d5cbee9d8683f4738988de5498f3ca38821
kyle <kyle.chang@ackuretta.com>
2025-09-03 18:22:35 +0800

    chore: update imports with path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,12 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/admin/, ''),
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@pages': resolve(__dirname, 'src/pages/index.ts'),
     }
   }
 })
