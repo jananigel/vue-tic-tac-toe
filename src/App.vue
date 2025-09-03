3308e90ab55654bbc5a818db06f0f0949f62f097
kyle <kyle.chang@ackuretta.com>
2025-09-03 16:39:26 +0800

    feat: init the project
    1. Set the tailwind css

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue + Tailwind CSS" />
+</template>
+
+<style scoped lang="scss"></style>
